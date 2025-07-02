3b1140eb386717a1d9112077aec71a0d2ffa700c
sherrlockkk <yasineren.sahin007@gmail.com>
2025-07-02 15:05:34 +0300

    Inıtial Commit

--- NorthwindApp.API/Program.cs
+++ NorthwindApp.API/Program.cs
@@ -1,20 +1,21 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using NorthwindApp.Data.Context;
+using NorthwindApp.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddNorthwindData(builder.Configuration);
 
-builder.Services.AddDbContext<NorthwindContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindConnection")));
-
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ NorthwindApp.Data/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthwindApp.Data.Context;
+
+namespace NorthwindApp.Data.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddNorthwindData(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<NorthwindContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("NorthwindConnection"));
+            });
+
+            // Register your data services here
+            // Example: services.AddScoped<IProductService, ProductService>();
+
+            return services;
+        }
+    }
+}
